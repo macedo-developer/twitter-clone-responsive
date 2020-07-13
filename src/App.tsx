caeadc1050d39eecd75549955f51683dc59363ab
macedo-developer <macedorenata.developer@gmail.com>
2020-07-12 22:45:51 -0300

    Created Header Main

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import GlobalStyles from './styles/GlobalStyles';
 
+import Layout from './components/Layout';
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Layout />
       <GlobalStyles />
     </>
   );
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
+import { Container, Header, BackIcon, ProfileInfo } from './style';
+
 const Main: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon />
+        </button>
+        <ProfileInfo>
+          <strong>Renata Macedo</strong>
+          <span>34 Tweets</span>
+        </ProfileInfo>
+      </Header>
+      {/* <ProfilePage/> */}
+      {/* <BottonMenu>
+           <HomeIcon/>
+           <SearchIcon/>
+           <BellIcon/>
+           <EmailIcon/>
+      </BottonMenu> */}
+    </Container>
+  );
 };
 
 export default Main;
